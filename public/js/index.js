19ef8bd8205f480e516c3828c4ef0d6e2c400024
akshayr11 <akshay.rajput5394@gmail.com>
2018-11-28 17:42:40 +0530

    ui for chat app

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,38 @@
 var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to the server');
-	socket.on('newMessage', function(data) {
-		console.log('New Message', JSON.stringify(data, undefined, 2));
+	socket.on('newMessage', function(message) {
+		const li = document.createElement('div');
+		var textnode = document.createTextNode(`${message.from}: ${message.text} `);
+		li.appendChild(textnode);
+		var list = document.getElementById('messages');
+		list.insertBefore(li, list.childNodes[0]);
 	});
 });
 socket.on('disconnect', function() {
 	console.log('Disonnected from the server');
 });
-socket.emit(
-	'createMessage',
-	{
-		from: 'Akshay',
-		text: 'Hi!!'
-	},
-	function(data) {
-		console.log('Got It', data);
-	}
-);
+// socket.emit(
+// 	'createMessage',
+// 	{
+// 		from: 'Akshay',
+// 		text: 'Hi!!'
+// 	},
+// 	function(data) {
+// 		console.log('Got It', data);
+// 	}
+// );
+document.getElementById('message-form').addEventListener('submit', function(event) {
+	event.preventDefault();
+	const message = document.getElementById('message-form').elements[0].value;
+	socket.emit(
+		'createMessage',
+		{
+			from: 'User',
+			text: message
+		},
+		function(data) {
+			console.log('Got It', data);
+		}
+	);
+});
