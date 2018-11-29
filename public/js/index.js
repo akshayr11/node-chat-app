9ae8a63564f4c9b38dd5e73c1ee9b9ab2bb4f948
akshayr11 <akshay.rajput5394@gmail.com>
2018-11-29 15:13:54 +0530

    UI for chat Application

--- public/js/index.js
+++ public/js/index.js
@@ -2,26 +2,15 @@ var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to the server');
 	socket.on('newMessage', function(message) {
-		const li = document.createElement('div');
-		var textnode = document.createTextNode(`${message.from}: ${message.text} `);
-		li.appendChild(textnode);
-		var list = document.getElementById('messages');
-		list.insertBefore(li, list.childNodes[0]);
+		const chatBox = document.querySelector('ul');
+		const newMessage = document.createElement('li');
+		newMessage.textContent = `${message.from}: ${message.text} `;
+		chatBox.appendChild(newMessage, chatBox.firstElementChild);
 	});
 });
 socket.on('disconnect', function() {
 	console.log('Disonnected from the server');
 });
-// socket.emit(
-// 	'createMessage',
-// 	{
-// 		from: 'Akshay',
-// 		text: 'Hi!!'
-// 	},
-// 	function(data) {
-// 		console.log('Got It', data);
-// 	}
-// );
 document.getElementById('message-form').addEventListener('submit', function(event) {
 	event.preventDefault();
 	const message = document.getElementById('message-form').elements[0].value;
